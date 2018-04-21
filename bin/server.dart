f1a92ecb1db4406906c54797b09b15b76ed88e07
Fred Luddy <fred@n4fl.com>
2018-04-21 11:46:29 -0700

    Initial commit

---
+++ bin/server.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:args/args.dart';
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_proxy/shelf_proxy.dart' as shelf_proxy;
+import 'package:shelf_route/shelf_route.dart';
+
+
+main(List<String> args) async {
+  var options = parseArgs(args);
+
+  // router
+  Router r = router(middleware: shelf.logRequests());
+  r.add('/echo', ['GET'], _echoRequest, exactMatch: false);
+  // pub serve for transform and static files out of working dir
+  var rph = await getPubProxy(options['pubport'], options['dir']);
+  r.add('/', ['GET'], rph, exactMatch: false);
+
+  io.serve(r.handler, '0.0.0.0', options['port']).then((server) {
+    print('Serving at http://${server.address.host}:${server.port}');
+  });
+}
+
+shelf.Response _echoRequest(shelf.Request request) {
+  return new shelf.Response.ok('Request for "${request.url}"');
+}
+
+// run pub serve and return a shelf proxy to the pub serve port
+Future<shelf.Handler> getPubProxy(port, workingDir) async {
+  Process p = await Process.start("pub", ["serve", "--port", "$port"], workingDirectory: workingDir);
+  p.stdout.transform(UTF8.decoder).transform(const LineSplitter()).listen((v) => print('[pub serve] $v'));
+  p.stderr.transform(UTF8.decoder).transform(const LineSplitter()).listen((v) => print('[pub serve] $v'));
+  p.exitCode.then((exitCode) {
+    print('pub serve exit code: $exitCode');
+  });
+
+  // under VSCODE, the child is sometimes left running without these handlers
+  killPubServe(ProcessSignal signal) {
+    print("Killing pub serve");
+    p.kill();
+    exit(0);
+  }
+
+  ProcessSignal.SIGTERM.watch().listen(killPubServe);
+  ProcessSignal.SIGINT.watch().listen(killPubServe);
+
+  print('Launched pub serve at port $port, pid ${p.pid}');
+  return shelf_proxy.proxyHandler(Uri.parse('http://localhost:$port'));
+}
+
+parseArgs(List<String> args) {
+  var options = {};
+  var parser = new ArgParser()
+    ..addOption('port', abbr: 'p', defaultsTo: '8080')
+    ..addOption('pubport', abbr: 'x', defaultsTo: '9090')
+    ..addOption('dir', abbr: 'd', defaultsTo: '.');
+
+  var t = parser.parse(args);
+  options['port'] = int.parse(t['port'], onError: (val) {
+    print('Could not parse port value "$val" into a number');
+    exit(1);
+  });
+
+  options['pubport'] = int.parse(t['pubport'], onError: (val) {
+    print('Could not parse port value "$val" into a number');
+    exit(1);
+  });
+
+  options['dir'] = t['dir'];
+  return options;
+}
---
+++ web/main.dart
@@ -0,0 +1,6 @@
+import 'dart:html';
+
+void main() {
+  var el = document.querySelector('#message');
+  el.text = 'main.dart locked and loaded :-)';
+}
